ef24e91ce30258e46b6e3955b8b0e963addce10f
Dillon <84485753+dillonjayatilake@users.noreply.github.com>
2025-10-07 15:00:39 +0530

    map system updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Login from './components/Login/Login'
 import CakeModel from './components/CakeModel/CakeModel'
+import Map from './components/MapSystem/map'
 
 function App() {
+
+
+
   return (
-    <div>
+     
+      <div>
       <CakeModel />
     </div>
+
   )
 }
 
